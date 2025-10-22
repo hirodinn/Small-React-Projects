6b1c776b11bae3d32a6e37f75c97ad2cfffbdcc0
hirodinn <hirebikila1@gmail.com>
2025-10-22 15:14:04 +0300

    make other implementation

--- react-1/src/components/project-6/EachFolder.jsx
+++ react-1/src/components/project-6/EachFolder.jsx
@@ -1,27 +1,26 @@
 import { useState } from "react";
-
 export function EachFolder({ folder }) {
   const [expanded, setExpanded] = useState(false);
-
   return (
-    <div style={{ marginLeft: 16 }}>
-      <div
-        style={{
-          cursor: "pointer",
-          fontWeight: folder.child?.length ? "bold" : "normal",
-        }}
-        onClick={() => setExpanded(!expanded)}
-      >
+    <div className="indent">
+      <div>
         {folder.name}
+        {folder.child.length > 0 && (
+          <button
+            onClick={() => {
+              setExpanded(!expanded);
+            }}
+          >
+            {expanded ? "-" : "+"}
+          </button>
+        )}
       </div>
 
       {expanded &&
         folder.child &&
         folder.child.map((child, index) =>
           typeof child === "string" ? (
-            <div key={index} style={{ marginLeft: 16 }}>
-              {child}
-            </div>
+            <div>{child}</div>
           ) : (
             <EachFolder key={index} folder={child} />
           )

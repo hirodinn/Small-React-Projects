b8e55d554a5cd9697ea7eb55c3e94e33c4ce1dfc
hirodinn <hirebikila1@gmail.com>
2025-10-23 21:29:43 +0300

    add the mark functionality

--- react-1/src/components/project-14/Project14.jsx
+++ react-1/src/components/project-14/Project14.jsx
@@ -2,16 +2,33 @@ import { useState } from "react";
 import "./Project14.css";
 export function Project14() {
   const [board, setBoard] = useState([
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
   ]);
+  const [currentSign, setCurrentSign] = useState("X");
+  function changeSign() {
+    if (currentSign === "X") setCurrentSign("O");
+    else setCurrentSign("X");
+  }
   return (
     <div className="project-14">
       <div className="board">
-        {board.map((row) => {
-          return row.map((box) => {
-            return <div className="box">{box}</div>;
+        {board.map((row, i) => {
+          return row.map((box, j) => {
+            return (
+              <div
+                className="box"
+                onClick={() => {
+                  const temp = board;
+                  temp[i][j] = currentSign;
+                  setBoard(temp);
+                  changeSign();
+                }}
+              >
+                {box}
+              </div>
+            );
           });
         })}
       </div>

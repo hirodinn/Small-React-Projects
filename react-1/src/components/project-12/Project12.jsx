e327e391cd51af2f8e632e5aa22d135191fa944b
hirodinn <hirebikila1@gmail.com>
2025-10-23 18:26:16 +0300

    install the dayjs package and finish the structure

--- react-1/src/components/project-12/Project12.jsx
+++ react-1/src/components/project-12/Project12.jsx
@@ -1,20 +1,29 @@
 import { useEffect, useState } from "react";
+import dayjs from "dayjs";
 import "./Project12.css";
 import axios from "axios";
 export function Project12() {
   const [userDetails, setUserDetails] = useState(null);
   const [inputValue, setInputValue] = useState(" ");
   const [isLoading, setIsLoading] = useState(true);
+  const [loaded, setLoaded] = useState(false);
   const loadData = async () => {
     setIsLoading(true);
-    const response = await axios.get(
-      `https://api.github.com/users/${
-        inputValue.trim() === "" ? "hirodinn" : inputValue.trim()
-      }`
-    );
-    setUserDetails(response.data);
-    setInputValue("");
-    setIsLoading(false);
+    setLoaded(false);
+    try {
+      const response = await axios.get(
+        `https://api.github.com/users/${
+          inputValue.trim() === "" ? "hirodinn" : inputValue.trim()
+        }`
+      );
+      setUserDetails(response.data);
+      setInputValue("");
+      setLoaded(true);
+    } catch {
+      setLoaded(false);
+    } finally {
+      setIsLoading(false);
+    }
   };
   useEffect(() => {
     loadData();
@@ -35,8 +44,33 @@ export function Project12() {
               }}
               placeholder="search for github account"
             />
-            <button>Submit</button>
+            <button
+              onClick={() => {
+                loadData();
+              }}
+            >
+              Submit
+            </button>
           </form>
+          {loaded ? (
+            <div className="content-container">
+              <img src={userDetails.avatar_url} />
+              <div className="info">
+                <a href={userDetails.html_url} target="_blank">
+                  {userDetails.name}
+                </a>
+                <p>
+                  User Joined on{" "}
+                  {dayjs(userDetails.created_at).format("DD MMMM YYYY")}
+                </p>
+              </div>
+              <p>Public Repos {userDetails.public_repos}</p>
+              <p>Followers {userDetails.followers}</p>
+              <p>Following {userDetails.following}</p>
+            </div>
+          ) : (
+            <h1>User Not Found</h1>
+          )}
         </>
       )}
     </div>

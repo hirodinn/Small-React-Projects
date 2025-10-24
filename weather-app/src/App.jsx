de762ccc9b0bcfbc6f5cf3b5749252b3ead41274
hirodinn <hirebikila1@gmail.com>
2025-10-25 00:33:14 +0300

    add the functionality of search button

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -4,19 +4,25 @@ import dayjs from "dayjs";
 export default function App() {
   const [inputValue, setInputValue] = useState("");
   const [weatherDetails, setWeatherDetails] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [successful, setSuccessful] = useState(true);
   const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
   async function loadData() {
     try {
+      setIsLoading(true);
       const response = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?q=${
-          inputValue.trim() === "" ? "London" : inputValue.trim()
+          inputValue.trim() === "" ? "Addis Ababa" : inputValue.trim()
         }&appid=${apiKey}&units=metric`
       );
       setWeatherDetails(response.data);
       console.log(response.data);
+      setSuccessful(true);
     } catch (error) {
       console.error("Error fetching weather:", error);
-      setWeatherDetails(null);
+      setSuccessful(false);
+    } finally {
+      setIsLoading(false);
     }
   }
   useEffect(() => {
@@ -25,37 +31,52 @@ export default function App() {
   }, []);
   return (
     <div className="container">
-      {weatherDetails && (
-        <>
-          <form>
-            <input
-              type="text"
-              placeholder="Enter City Name"
-              value={inputValue}
-              onChange={(e) => {
-                setInputValue(e.target.value);
-              }}
-            />
-            <button>Search</button>
-          </form>
-          <h2>
-            {weatherDetails.name}, {weatherDetails.sys.country}
-          </h2>
-          <p className="date">{dayjs().format("dddd, MMMM DD, YYYY ")}</p>
-          <h1>283.63</h1>
-          <p>Overcast {weatherDetails.weather[0].main}</p>
-          <div className="weather-info">
-            <div className="wind">
-              <p>{weatherDetails.wind.speed}</p>
-              <p>Wind Speed</p>
-            </div>
-            <div className="humidity">
-              <p>{weatherDetails.main.humidity}%</p>
-              <p>Humidity</p>
-            </div>
+      {weatherDetails &&
+        (isLoading ? (
+          <div className="loading">
+            <div className="circle"></div>
+            <p>Loading</p>
           </div>
-        </>
-      )}
+        ) : (
+          <>
+            <form>
+              <input
+                type="text"
+                placeholder="Enter City Name"
+                value={inputValue}
+                onChange={(e) => {
+                  setInputValue(e.target.value);
+                  setSuccessful(true);
+                }}
+              />
+              <button onClick={loadData}>Search</button>
+            </form>
+            {successful ? (
+              <>
+                <h2>
+                  {weatherDetails.name}, {weatherDetails.sys.country}
+                </h2>
+                <p className="date">{dayjs().format("dddd, MMMM DD, YYYY ")}</p>
+                <h1>283.63</h1>
+                <p>Overcast {weatherDetails.weather[0].main}</p>
+                <div className="weather-info">
+                  <div className="wind">
+                    <p>{weatherDetails.wind.speed}</p>
+                    <p>Wind Speed</p>
+                  </div>
+                  <div className="humidity">
+                    <p>{weatherDetails.main.humidity}%</p>
+                    <p>Humidity</p>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <div className="no-info">
+                <p>No weather info about {inputValue}</p>
+              </div>
+            )}
+          </>
+        ))}
     </div>
   );
 }

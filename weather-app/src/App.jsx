1099bd07b5a8fed5385c850d683696bf43b5d690
hirodinn <hirebikila1@gmail.com>
2025-10-24 23:35:33 +0300

    finish styling the form

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <form>
+        <input type="text" placeholder="Enter City Name" />
+        <button>Search</button>
+      </form>
+      <h3>London, GB</h3>
+      <h2>sunday, December 17, 2023</h2>
+      <p>Overcast clouds</p>
+      <div className="weather-info">
+        <div className="wind">
+          <p>4.63</p>
+          <p>Wind Speed</p>
+        </div>
+        <div className="humidity">
+          <p>83%</p>
+          <p>Humidity</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App

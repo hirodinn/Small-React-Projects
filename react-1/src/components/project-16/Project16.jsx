b1b621febca04c7808bb17e08c7531cc9b5e856e
hirodinn <hirebikila1@gmail.com>
2025-10-24 22:08:18 +0300

    add the click hook

--- react-1/src/components/project-16/Project16.jsx
+++ react-1/src/components/project-16/Project16.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import "./Project16.css";
 export function Project16({ windowDimension }) {
+  const [outsideClick, setOutsideClick] = useState(true);
   return (
-    <div className="project-16">
-      <div>width : {windowDimension[0]}</div>
-      <div>height : {windowDimension[1]}</div>
+    <div
+      className="project-16"
+      onClick={() => {
+        if (!outsideClick) setOutsideClick(true);
+      }}
+    >
+      <h1>Use Outside Click Hook</h1>
+      <div className="outside-click-hook">
+        {outsideClick ? (
+          <button
+            onClick={() => {
+              setOutsideClick(false);
+            }}
+          >
+            Show Content
+          </button>
+        ) : (
+          <div className="content">this is clicked</div>
+        )}
+      </div>
+      <h1>Use Resize Hook</h1>
+      <div className="resize-hook">
+        <div>width : {windowDimension[0]}</div>
+        <div>height : {windowDimension[1]}</div>
+      </div>
     </div>
   );
 }

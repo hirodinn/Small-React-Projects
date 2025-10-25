066d7b79558dde702a7cb150a678f43c4f4141d0
hirodinn <hirebikila1@gmail.com>
2025-10-25 14:45:18 +0300

    done the structure for the recipe container

--- food-recipe/src/components/Main.jsx
+++ food-recipe/src/components/Main.jsx
@@ -1,10 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Header } from "./Header";
+import "./Main.css";
 
 export function Main() {
+  const [recipes, setRecipes] = useState([]);
+  async function loadData() {
+    const response = await axios.get(
+      "https://forkify-api.herokuapp.com/api/v2/recipes?search=banana"
+    );
+
+    setRecipes(response.data.data.recipes);
+  }
+  useEffect(() => {
+    loadData();
+  }, []);
   return (
     <main>
       <Header />
-      <h1>This is main</h1>
+      <div className="recipe-container">
+        {recipes.map((recipe, i) => {
+          return (
+            <div className="each-recipe" key={i}>
+              <img src={recipe.image_url} />
+              <p className="publisher">recipe.publisher</p>
+              <p className="title">recipe.title</p>
+              <button>Recipe Details</button>
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 }

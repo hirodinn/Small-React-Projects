a753a093dd407c86a95f159dbb0e88d0545241dd
hirodinn <hirebikila1@gmail.com>
2025-10-25 16:27:12 +0300

    add the class to favorites which treats as the container in the main page

--- food-recipe/src/components/Favorites.jsx
+++ food-recipe/src/components/Favorites.jsx
@@ -1,10 +1,11 @@
 import { Header } from "./Header";
 import { NavLink } from "react-router-dom";
+import "./Favorites.css";
 export function Favorites({ favorites }) {
   return (
     <>
       <Header />
-      <div className="favorites-container">
+      <div className="recipe-container">
         {favorites.length > 0 &&
           favorites.map((recipe, i) => {
             return (

0bd8c2a5c99ed7492c954429144f5289f87bcb1c
hirodinn <hirebikila1@gmail.com>
2025-10-28 20:40:29 +0300

    Connect dropdown to FruitCard display

--- fruit-classifier/src/App.jsx
+++ fruit-classifier/src/App.jsx
@@ -1,5 +1,30 @@
-function App() {
-  return <></>;
-}
+import { useState } from "react";
+import { fruits } from "./data";
+import FruitCard from "./components/FruitCard";
+import "./App.css";
 
-export default App;
+export default function App() {
+  const [selectedFruit, setSelectedFruit] = useState(null);
+
+  const handleChange = (e) => {
+    const fruit = fruits.find((f) => f.name === e.target.value);
+    setSelectedFruit(fruit);
+  };
+
+  return (
+    <div className="App">
+      <h1>Fruit Classifier</h1>
+      <select onChange={handleChange} defaultValue="">
+        <option value="" disabled>
+          Select a fruit
+        </option>
+        {fruits.map((f) => (
+          <option key={f.name} value={f.name}>
+            {f.name}
+          </option>
+        ))}
+      </select>
+      <FruitCard fruit={selectedFruit} />
+    </div>
+  );
+}

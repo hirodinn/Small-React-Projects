cfb58cbd0fdb617c55fd6cb7bcb4214ca7d65819
hirodinn <hirebikila1@gmail.com>
2025-10-28 20:38:26 +0300

    Add data.js with fruit data

---
+++ fruit-classifier/src/data.js
@@ -0,0 +1,32 @@
+export const fruits = [
+  {
+    name: "Apple",
+    type: "Pome",
+    color: "Red/Green",
+    description: "Sweet and crunchy.",
+  },
+  {
+    name: "Banana",
+    type: "Berry",
+    color: "Yellow",
+    description: "Soft and sweet.",
+  },
+  {
+    name: "Orange",
+    type: "Citrus",
+    color: "Orange",
+    description: "Juicy and tangy.",
+  },
+  {
+    name: "Strawberry",
+    type: "Aggregate",
+    color: "Red",
+    description: "Sweet and juicy.",
+  },
+  {
+    name: "Grapes",
+    type: "Berry",
+    color: "Purple/Green",
+    description: "Small and sweet.",
+  },
+];

d0891bdf0f42b68fea4cedd82777d199da5607dc
hirodinn <hirebikila1@gmail.com>
2025-11-03 23:40:53 +0300

    add the hover state

--- tailwind-css/src/App.jsx
+++ tailwind-css/src/App.jsx
@@ -5,12 +5,12 @@ function App() {
 
   return (
     <div
-      className={` bg-(--primary-background) h-screen flex justify-center items-center relative ${
+      className={`bg-(--primary-background) h-screen flex justify-center items-center relative transition-all duration-1000 ${
         darkMode && "dark"
       }`}
       onClick={() => setDarkMode(!darkMode)}
     >
-      <div className="bg-(--box-1) w-[250px] text-box-2 h-[100px] absolute top-(--vertical-center-absolute) translate-y-(--vertical-translate)">
+      <div className="bg-(--box-1) w-[250px] text-box-2 h-[100px] absolute top-(--vertical-center-absolute) translate-y-(--vertical-translate) hover:scale-150 transition-all duration-300">
         Hi my name is hire bikila
       </div>
       <div className="bg-(--box-2) w-[250px] h-[100px] "></div>

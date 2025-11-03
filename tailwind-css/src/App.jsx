1a51222b8ee53183713b8672835e0bcdbcae3420
hirodinn <hirebikila1@gmail.com>
2025-11-03 22:42:48 +0300

    use the position absolute

--- tailwind-css/src/App.jsx
+++ tailwind-css/src/App.jsx
@@ -5,19 +5,15 @@ function App() {
 
   return (
     <div
-      className={` bg-(--primary-background) h-screen flex justify-center items-center ${
+      className={` bg-(--primary-background) h-screen flex justify-center items-center relative ${
         darkMode && "dark"
       }`}
       onClick={() => setDarkMode(!darkMode)}
     >
-      <div
-        className={`bg-(--box-1) w-[250px] text-box-2 h-[100px] animate-loadInfinite`}
-      >
+      <div className="bg-(--box-1) w-[250px] text-box-2 h-[100px] absolute top-(--vertical-center-absolute) translate-y-(--vertical-translate)">
         Hi my name is hire bikila
       </div>
-      <div
-        className={`bg-(--box-2) w-[250px] h-[100px] animate-loadInfinite`}
-      ></div>
+      <div className="bg-(--box-2) w-[250px] h-[100px] "></div>
     </div>
   );
 }

cfabe46c93cf3e910926743656e98b005d0b40b7
hirodinn <hirebikila1@gmail.com>
2025-11-03 18:48:52 +0300

    add the transition to the boxes trial

--- tailwind-css/src/App.jsx
+++ tailwind-css/src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 function App() {
+  const [spaceBetween, setSpaceBetween] = useState(true);
+
   return (
-    <div className="text-8xl bg-amber-500 text-orange-600">
-      My first tailwind css file
+    <div
+      className="text-8xl bg-gray-200 text-orange-600 h-screen flex justify-center items-center"
+      onClick={() => setSpaceBetween(!spaceBetween)}
+    >
+      <div
+        className={`bg-blue-400 w-[250px] h-[100px] transition-all duration-1000`}
+        style={{ marginRight: spaceBetween ? "20rem" : "1rem" }}
+      ></div>
+      <div
+        className={`bg-red-400 w-[250px] h-[100px] transition-all duration-1000`}
+        style={{ marginLeft: spaceBetween ? "20rem" : "1rem" }}
+      ></div>
     </div>
   );
 }

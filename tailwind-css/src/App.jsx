15cf5c5a3ebb033a7be8fa60cfae5c0a7a57278a
hirodinn <hirebikila1@gmail.com>
2025-11-04 10:33:17 +0300

    add the image

--- tailwind-css/src/App.jsx
+++ tailwind-css/src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <div className="font-youngserif text-6xl font">hi this is font</div>;
+  return (
+    <div className="bg-(--primary-background) h-screen w-screen flex items-center justify-center">
+      <div className="bg-white w-[95vw] max-w-[500px] flex flex-col p-5 rounded-[10px]">
+        <img src="images/image-omelette.jpeg" />
+      </div>
+    </div>
+  );
 }
 
 export default App;

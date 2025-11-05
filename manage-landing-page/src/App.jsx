35654be03152b594aae5c538511a478282414200
hirodinn <hirebikila1@gmail.com>
2025-11-05 15:43:46 +0300

    add the statistics

--- manage-landing-page/src/App.jsx
+++ manage-landing-page/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className=" flex felx-col p-[50px] h-[200vw] font-['Outfit']">
-      <header className="w-[90%] bg-amber-200 h-20 mx-auto sticky top-2.5 z-10 flex justify-between items-center max-w-[1200px] px-7">
+    <div className=" flex flex-col py-6 h-fit font-['Outfit'] bg-[url(public/images/bg-simplify-section-desktop.svg)] bg-no-repeat fill-amber-300 px-5 xl:px-40">
+      <header className="w-full h-20 mx-auto flex justify-between items-center">
         <img src="images/logo.svg" className="cursor-pointer" />
         <ul className="md:flex items-center justify-center hidden">
           <li className="pr-8 cursor-pointer hover:text-gray-500">Pricing</li>
@@ -12,12 +12,29 @@ function App() {
         </ul>
         <img
           src="images/icon-hamburger.svg"
-          className="pr-10 block md:hidden"
+          className="block md:hidden cursor-pointer"
         />
         <button className="rounded-[30px] cursor-pointer text-white bg-my-orange px-5 py-2.5 hover:bg-my-orange-hover hidden md:block ">
           Get Started
         </button>
       </header>
+      <div className="flex gap-[10%] flex-col-reverse lg:flex-row mt-5">
+        <div className="flex-1 py-8 flex flex-col items-center lg:items-start text-center lg:text-start justify-around">
+          <h1 className="text-7xl text-my-blue font-bold">
+            Bring everyone together to build better products
+          </h1>
+          <p className="my-7 max-w-[78%] text-2xl text-gray-500">
+            Manage makes it simple for software teams to plan day-to-day tasks
+            while keeping the larger teams goals in view
+          </p>
+          <button className="rounded-[30px] cursor-pointer text-white bg-my-orange px-5 py-2.5 hover:bg-my-orange-hover ">
+            Get Started
+          </button>
+        </div>
+        <div className="flex-1 flex justify-center lg:justify-end">
+          <img src="public/images/illustration-intro.svg" className="h-fit" />
+        </div>
+      </div>
     </div>
   );
 }

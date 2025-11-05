8d55f4f3b68fa2977778e8a341b88ba2acc112b0
hirodinn <hirebikila1@gmail.com>
2025-11-05 16:11:05 +0300

    add the about us

--- manage-landing-page/src/App.jsx
+++ manage-landing-page/src/App.jsx
@@ -35,6 +35,65 @@ function App() {
           <img src="public/images/illustration-intro.svg" className="h-fit" />
         </div>
       </div>
+      <div className="flex flex-col gap-5 lg:flex-row mt-5">
+        <div className="flex-1 flex flex-col items-center lg:items-start text-center lg:text-start mb-10 lg:mb-0">
+          <h1 className="text-5xl text-my-blue font-bold max-w-[80%] mb-5">
+            What's different about Manage?
+          </h1>
+          <p className="text-2xl text-gray-500 max-w-[80%] leading-loose lg:leading-relaxed">
+            Manage provides all the functionality your teams, without the
+            complexity. Our software is tailor-made for modern digital products
+          </p>
+        </div>
+        <div className="flex-1 px-6">
+          <div className="flex items-start gap-4 mb-4">
+            <div className="text-white bg-my-orange px-8 py-2.5 rounded-3xl">
+              01
+            </div>
+            <div>
+              <h2 className="font-bold text-3xl text-my-blue mb-3">
+                Track company-wide progress
+              </h2>
+              <p className="text-2xl text-gray-500">
+                See how your day-to-day tasks fit into the wider vision. Go from
+                tracking progress at the milestone level all the way down to the
+                smallest of details. Never lose sight of the bigger picture
+                again.
+              </p>
+            </div>
+          </div>
+          <div className="flex items-start gap-4 mb-4">
+            <div className="text-white bg-my-orange px-8 py-2.5 rounded-3xl">
+              02
+            </div>
+            <div>
+              <h2 className="font-bold text-3xl text-my-blue mb-3">
+                Advanced built-in reports
+              </h2>
+              <p className="text-2xl text-gray-500">
+                Set internal delivery estimates and track progress toward
+                company goals. Our customisable dashboard helps you build out
+                the reports you need to keep key stakeholders informed.
+              </p>
+            </div>
+          </div>
+          <div className="flex items-start gap-4 mb-4">
+            <div className="text-white bg-my-orange px-8 py-2.5 rounded-3xl">
+              03
+            </div>
+            <div>
+              <h2 className="font-bold text-3xl text-my-blue mb-3">
+                Everything you need in one place
+              </h2>
+              <p className="text-2xl text-gray-500">
+                Stop jumping from one service to another to communicate, store
+                files, track tasks and share documents. Manage offers an
+                all-in-one team productivity solution.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

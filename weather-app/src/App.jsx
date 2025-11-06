fe9ccb1938ba79eee6019ac8791dea7d865ab481
hirodinn <hirebikila1@gmail.com>
2025-11-06 14:12:58 +0300

    the images and contain the container inside another container to change background

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -29,53 +29,57 @@ export default function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
-    <div className="container">
-      {weatherDetails &&
-        (isLoading ? (
-          <div className="loading">
-            <div className="circle"></div>
-            <p>Loading</p>
-          </div>
-        ) : (
-          <>
-            <form>
-              <input
-                type="text"
-                placeholder="Enter City Name"
-                value={inputValue}
-                onChange={(e) => {
-                  setInputValue(e.target.value);
-                  setSuccessful(true);
-                }}
-              />
-              <button onClick={loadData}>Search</button>
-            </form>
-            {successful ? (
-              <>
-                <h2>
-                  {weatherDetails.name}, {weatherDetails.sys.country}
-                </h2>
-                <p className="date">{dayjs().format("dddd, MMMM DD, YYYY ")}</p>
-                <h1>{weatherDetails.main.temp}</h1>
-                <p>{weatherDetails.weather[0].description}</p>
-                <div className="weather-info">
-                  <div className="wind">
-                    <p>{weatherDetails.wind.speed}</p>
-                    <p>Wind Speed</p>
+    <div className="body">
+      <div className="container">
+        {weatherDetails &&
+          (isLoading ? (
+            <div className="loading">
+              <div className="circle"></div>
+              <p>Loading</p>
+            </div>
+          ) : (
+            <>
+              <form>
+                <input
+                  type="text"
+                  placeholder="Enter City Name"
+                  value={inputValue}
+                  onChange={(e) => {
+                    setInputValue(e.target.value);
+                    setSuccessful(true);
+                  }}
+                />
+                <button onClick={loadData}>Search</button>
+              </form>
+              {successful ? (
+                <>
+                  <h2>
+                    {weatherDetails.name}, {weatherDetails.sys.country}
+                  </h2>
+                  <p className="date">
+                    {dayjs().format("dddd, MMMM DD, YYYY ")}
+                  </p>
+                  <h1>{weatherDetails.main.temp}</h1>
+                  <p>{weatherDetails.weather[0].description}</p>
+                  <div className="weather-info">
+                    <div className="wind">
+                      <p>{weatherDetails.wind.speed}</p>
+                      <p>Wind Speed</p>
+                    </div>
+                    <div className="humidity">
+                      <p>{weatherDetails.main.humidity}%</p>
+                      <p>Humidity</p>
+                    </div>
                   </div>
-                  <div className="humidity">
-                    <p>{weatherDetails.main.humidity}%</p>
-                    <p>Humidity</p>
-                  </div>
+                </>
+              ) : (
+                <div className="no-info">
+                  <p>No weather info about {inputValue}</p>
                 </div>
-              </>
-            ) : (
-              <div className="no-info">
-                <p>No weather info about {inputValue}</p>
-              </div>
-            )}
-          </>
-        ))}
+              )}
+            </>
+          ))}
+      </div>
     </div>
   );
 }

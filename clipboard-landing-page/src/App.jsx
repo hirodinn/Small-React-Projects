d91e2b30cc1afa8bb2d9747d3b90d940666d9482
hirodinn <hirebikila1@gmail.com>
2025-11-11 22:56:28 +0300

    add the third section

--- clipboard-landing-page/src/App.jsx
+++ clipboard-landing-page/src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 function App() {
   return (
-    <div className="font-outfit">
-      <div className=" bg-[url(/images/bg-header-mobile.png)] md:bg-[url(/images/bg-header-desktop.png)] bg-no-repeat bg-cover h-150 md:h-100 flex items-center justify-center">
+    <div className="font-outfit box-border h-fit w-full">
+      <div className=" bg-[url(/images/bg-header-mobile.png)] md:bg-[url(/images/bg-header-desktop.png)] bg-no-repeat bg-cover h-150 md:h-100 flex items-center justify-center box-border">
         <img src="images/logo.svg" className="min-w-[300px] md:min-w-0" />
       </div>
-      <div className="w-[80%] max-w-[750px] mx-auto mt-[-70px] md:mt-0">
+      <div className="w-[80%] max-w-[750px] mx-auto mt-[-70px] md:mt-0 mb-10 box-border">
         <h1 className="text-my-dark-gray text-center text-5xl font-bold">
           A history of everything you copy
         </h1>
@@ -29,6 +29,40 @@ function App() {
           can access your snippets immediately on all your devices. Our Mac and
           iOS apps will help you organize everything.
         </p>
+      </div>
+      <div className="flex flex-col md:flex-row box-border border-2 h-fit mb-12 max-w-full">
+        <img
+          src="images/image-computer.png"
+          className="relative right-20 w-1/2 h-fit self-center"
+        />
+        <div className="flex flex-col box-border ml-[5vw] max-w-[500px] border-2 min-h-full justify-around">
+          <div className="w-full">
+            <h1 className="text-my-dark-gray text-4xl font-bold">
+              Quick Search
+            </h1>
+            <p className="mt-2 text-my-light-gray text-[20px]">
+              Easily search your snippets by content, category, web address,
+              application, and more
+            </p>
+          </div>
+          <div className="w-full">
+            <h1 className="text-my-dark-gray text-4xl font-bold">
+              iCloud Sync
+            </h1>
+            <p className="mt-2 text-my-light-gray text-[20px]">
+              instantly saves and syncs snippets across all your devices
+            </p>
+          </div>
+          <div className="w-full">
+            <h1 className="text-my-dark-gray text-4xl font-bold">
+              Compelete History
+            </h1>
+            <p className="mt-2 text-my-light-gray text-[20px]">
+              Retrive any snippets from the first moment you started using the
+              app.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );

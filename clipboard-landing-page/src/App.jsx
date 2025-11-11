92d90381d1c1ba6e98c7921b1ad48549117aff43
hirodinn <hirebikila1@gmail.com>
2025-11-11 22:04:17 +0300

    add font family

--- clipboard-landing-page/src/App.jsx
+++ clipboard-landing-page/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 function App() {
   return (
-    <div>
+    <div className="font-outfit">
       <div className=" bg-[url(/images/bg-header-mobile.png)] md:bg-[url(/images/bg-header-desktop.png)] bg-no-repeat bg-cover h-150 md:h-100 flex items-center justify-center">
         <img src="images/logo.svg" className="min-w-[300px] md:min-w-0" />
       </div>

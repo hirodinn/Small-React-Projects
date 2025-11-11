ec1f7a98527cc2ed5b4b665f7f819afa49312472
hirodinn <hirebikila1@gmail.com>
2025-11-11 21:36:22 +0300

    add a responsive header for the project

--- clipboard-landing-page/src/App.jsx
+++ clipboard-landing-page/src/App.jsx
@@ -1,6 +1,12 @@
 import "./App.css";
 function App() {
-  return <div className=""></div>;
+  return (
+    <div>
+      <div className=" bg-[url(/images/bg-header-mobile.png)] md:bg-[url(/images/bg-header-desktop.png)] bg-no-repeat bg-cover h-150 md:h-100 flex items-center justify-center">
+        <img src="images/logo.svg" className="min-w-[300px] md:min-w-0" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
